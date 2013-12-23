da11e049408f87f0b26fc0ce4bc4108aecf0fd40
yairmela <yair@playtika.com>
2013-12-23 11:11:09 +0200

    update Gamefile.lock for Heroko

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RirstApp::Application.config.secret_key_base = 'a12d7e463b2efed94c550888aa750cc1ae977c415cdd21cfb95534135b67d25abe4a4e8c5966f2ccc6e68332a5eac9f945d8546475c0c5cb41ef4e111233b4f5'
