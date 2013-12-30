e9e70c9dcf0227f9e5c6ad61b99788ef680af408
yairmela <yair@playtika.com>
2013-12-30 11:26:47 +0200

    finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RirstApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
  # get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
